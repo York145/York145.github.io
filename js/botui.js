86d121fbffcbd6e483888e62ce159e099a98de9e
York145 <2192557290@qq.com>
2020-10-06 18:11:54 +0800

    Site updated: 2020-10-06 18:11:50

--- js/botui.js
+++ js/botui.js
@@ -26,7 +26,7 @@ function bot_ui_ini() {
     }).then(function () {
         botui.message.add({
             delay: 1100,
-            content: "这里是 Mashiro"
+            content: "这里是小叹"
         }).then(function () {
             botui.message.add({
                 delay: 1100,
@@ -65,11 +65,11 @@ function bot_ui_ini() {
         secondpart = function () {
             botui.message.add({
                 delay: 1500,
-                content: "目前就读于上海财经大学"
+                content: "目前就读于温州大学"
             }).then(function () {
                 botui.message.add({
                     delay: 1500,
-                    content: "向往技术却误入商科，但后来喜欢上了经济学…"
+                    content: "向往技术却误入网安…"
                 }).then(function () {
                     botui.message.add({
                         delay: 1200,
@@ -77,21 +77,21 @@ function bot_ui_ini() {
                     }).then(function () {
                         botui.message.add({
                             delay: 1500,
-                            content: "主攻 R 语言和 Python，略懂 STATA，偶尔也折腾 HTML/CSS/JavaScript/PHP"
+                            content: "主攻Java和web，略懂python，偶尔也折腾内网"
                         }).then(function () {
                             botui.message.add({
                                 delay: 1500,
-                                content: "研究的方向，是经济/金融方向的数据分析（data science）以及机器学习（machine learning）"
+                                content: "研究的方向，是网络工程和网络安全"
                             }).then(function () {
                                 botui.message.add({
                                     delay: 1800,
-                                    content: "喜欢画画，希望有一天能够被称为画师"
+                                    content: "喜欢打游戏"
                                 }).then(function () {
                                     botui.action.button({
                                         delay: 1100,
                                         action: [{
-                                            text: "为什么叫Mashiro呢？ 🤔",
-                                            value: "why-mashiro"
+                                            text: "为什么叫xiaotan呢？ 🤔",
+                                            value: "因为喜欢《惊悚乐园》"
                                         }]
                                     }).then(function (a) {
                                         thirdpart()
@@ -106,13 +106,13 @@ function bot_ui_ini() {
         thirdpart = function () {
             botui.message.add({
                 delay: 1E3,
-                content: "Mashiro以及站名都来自一部动画，因为和主角有一样的爱好~ 如果有兴趣可以找找首页上的视频~"
+                content: "xiaotan来自小说，如果有兴趣可以找找《惊悚乐园》~"
             }).then(function () {
                 botui.action.button({
                     delay: 1500,
                     action: [{
-                        text: "为什么是白猫呢？ 🤔",
-                        value: "why-cat"
+                        text: "why so serious ?",
+                        value: "why"
                     }]
                 }).then(function (a) {
                     fourthpart()
@@ -126,7 +126,7 @@ function bot_ui_ini() {
             }).then(function () {
                 botui.message.add({
                     delay: 1100,
-                    content: "而且我真的是猫控！"
+                    content: "而且我真的喜欢《惊悚乐园》！"
                 }).then(function () {
                     botui.action.button({
                         delay: 1500,
